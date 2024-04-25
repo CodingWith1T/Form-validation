bd4b55e5f7d7e61851077d95ea85c3fc29818458
CodingWith1T <117918080+CodingWith1T@users.noreply.github.com>
2024-04-25 05:20:08 -0400

    Update Handle.php

--- Handler/Handle.php
+++ Handler/Handle.php
@@ -1,40 +1,43 @@
 <?php
-    session_start();
-    include '../Helper/DataBase.php';
-    $emailValue = $_POST['email'];
-    $passwordValue = $_POST['password'];
-    $email = "";
-    $pass = "";
-    $sql = "select email, password from emp where email = '$emailValue' AND password='$passwordValue'";
-    $result = mysqli_query($conn,$sql);
-    if($emailValue!="" && $passwordValue!=""){
-        while($row = $result->fetch_assoc()){
-            $email = $row['email'];
-            $pass =  $row['password'];
-            if($_POST['email'] == $email && $_POST['password'] == $pass ){
-                session_start();
-                $_SESSION['login'] = true;
-                $_SESSION['email'] = $email;
-                echo "<script>window.location.href='../HTML/index.php'</script>";
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+        include '../Helper/DataBase.php';
+    
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $phone = $_POST['phone'];
+        echo $country = $_POST['country'];
+        echo $state = $_POST['state'];
+        echo $city = $_POST['city'];
+        
+        echo $radio = $_POST['radio'];
+
+        //Check  Whether email is Exists
+        $existsSql = "SELECT * FROM `emp` WHERE email='$email'";
+        $result = mysqli_query($conn, $existsSql);
+        $numExistRows = mysqli_num_rows($result);
+        if($numExistRows > 0){
+            echo "<script>alert('Email is Already Exists');</script>";
+        }
+        else{
+            $sql = "INSERT INTO `emp` (`name`, `email`, `password`, `phone`, `country`, `state`, `city`) VALUES ('$name', '$email', '$password', '$phone',  '$country','$state','$city')";
+            $result = mysqli_query($conn, $sql);
+            if ($result) {
+            echo "<script>alert('SignIn Successful please login for Further.');
+            window.location.href='../login.php'</script>";   
             }
         }
     }
-    else if($emailValue!=$email){
-        echo "<script>
-        alert('Please Enter Correct Email');
-        window.location.href='../Login.php';
-        </script>";
-    }
-    else if($passwordValue!=$pass){
-        echo "<script>
-        alert('Please Enter Correct Password');
-        window.location.href='../Login.php';
-        </script>";
-    }
-    else{
-        echo "<script>
-            alert('Please Enter Email and Password ');
-            window.location.href='../Login.php';
-            </script>";
-    }
 ?>
+
+
+
+
+
+
+
+
+
+
+
